5f32924f07ef0165bc5be688050ba6e20d5f022b
prak132 <rathoreprakhar1@gmail.com>
2024-12-15 22:19:31 -0800

    Updated GenericVerticalArmSubsystem to remove uneccesary feedforward component

--- src/frc846/include/frc846/robot/GenericVerticalArmSubsystem.h
+++ src/frc846/include/frc846/robot/GenericVerticalArmSubsystem.h
@@ -3,12 +3,11 @@
 #include <units/angle.h>
 #include <units/angular_velocity.h>
 #include <units/length.h>
+#include <units/mass.h>
 
 #include "frc846/control/base/motor_gains.h"
-#include "frc846/control/config/construction_params.h"
+#include "frc846/math/calculator.h"
 #include "frc846/math/constants.h"
-#include "frc846/robot/GenericSubsystem.h"
-#include "frc846/wpilib/846_units.h"
 
 namespace frc846::robot {
 
@@ -16,8 +15,8 @@ namespace frc846::robot {
 struct VerticalArmConfigs {
   units::kilogram_t arm_mass;
   units::inch_t center_of_mass;
-  frc846::control::base::MotorGains motor_gains;
   units::degree_t offset_angle = 0.0_deg;
+  frc846::control::base::MotorGains motor_gains;
   units::degree_t max_pos;
   units::degree_t min_pos;
   double peak_output_forward = 1.0;
@@ -25,13 +24,10 @@ struct VerticalArmConfigs {
 };
 
 template <class Readings, class Target>
-class GenericVerticalArmSubsystem
-    : public frc846::robot::GenericSubsystem<Readings, Target> {
+class GenericVerticalArmSubsystem : public frc846::math::Calculator {
  public:
-  GenericVerticalArmSubsystem(
-      VerticalArmConfigs& configs,
-      frc846::control::config::MotorConstructionParameters& constr_params)
-      : configs_(configs), constr_params_(constr_params) {}
+  GenericVerticalArmSubsystem(VerticalArmConfigs& configs)
+      : configs_(configs) {}
 
   double CalculateDutyCycle(units::degree_t arm_position,
                             units::degree_t target_arm_position,
@@ -41,24 +37,16 @@ class GenericVerticalArmSubsystem
         frc846::math::constants::physics.g *
         std::abs(units::math::cos(arm_position + configs_.offset_angle)
                      .to<double>());
-    units::newton_meter_t acceleration_torque =
-        constr_params_.rotational_inertia *
-        (current_velocity - previous_velocity) / 20.0_ms;
     units::degree_t position_error = target_arm_position - arm_position;
-    previous_velocity = current_velocity;
-    // torque feedforward
-    units::newton_meter_t total_torque = gravity_torque + acceleration_torque;
     double duty_cycle = configs_.motor_gains.calculate(
-        position_error.to<double>(), 0, current_velocity.to<double>(),
-        total_torque.to<double>());
+        position_error.to<double>(), 0.0, current_velocity.to<double>(),
+        gravity_torque.to<double>());
     return std::max(std::min(duty_cycle, configs_.peak_output_forward),
                     configs_.peak_output_reverse);
   }
 
  private:
   VerticalArmConfigs& configs_;
-  frc846::control::config::MotorConstructionParameters& constr_params_;
-  units::degrees_per_second_t previous_velocity = 0.0_deg_per_s;
 };
 
 }  // namespace frc846::robot
