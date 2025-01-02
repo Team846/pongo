464066c9341a4473854d4a56e96b9e0922a226df
VyaasBaskar <vyaasbaskar@gmail.com>
2025-01-01 21:55:41 -0800

    Tested code with motor physics simulation

--- src/frc846/include/frc846/control/hardware/simulation/SIMLEVEL.h
+++ src/frc846/include/frc846/control/hardware/simulation/SIMLEVEL.h
@@ -5,6 +5,6 @@ namespace frc846::control::simulation {
 #define MOTOR_SIM_LEVEL_SIM_HARDWARE 1
 #define MOTOR_SIM_LEVEL_SIM_PHYSICS 2
 
-#define MOTOR_SIM_LEVEL MOTOR_SIM_LEVEL_SIM_HARDWARE
+#define MOTOR_SIM_LEVEL MOTOR_SIM_LEVEL_SIM_PHYSICS
 
 }  // namespace frc846::control::simulation
