bb17e134ddc7b3567fb3c3f397872cbf19dd5d34
VyaasBaskar <vyaasbaskar@gmail.com>
2025-01-14 15:10:55 -0800

    Integrated anti-tipping acceleration limiting

---
+++ src/frc846/cpp/frc846/math/RampRateLimiter.cc
@@ -0,0 +1,37 @@
+#include "frc846/math/RampRateLimiter.h"
+
+namespace frc846::math {
+
+RampRateLimiter::RampRateLimiter() {
+  m_lastTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+      std::chrono::system_clock::now().time_since_epoch());
+}
+
+double RampRateLimiter::limit(double value, double rateLimit) {
+  auto currentTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+      std::chrono::system_clock::now().time_since_epoch());
+  auto elapsedTime = currentTime - m_lastTime;
+  m_lastTime = currentTime;
+
+  double maxChange = rateLimit * elapsedTime.count() / 1000.0;
+  double change = value - m_lastValue;
+  if (change > maxChange) {
+    value = m_lastValue + maxChange;
+  } else if (change < -maxChange) {
+    value = m_lastValue - maxChange;
+  }
+
+  m_lastValue = value;
+  return value;
+}
+
+double RampRateLimiter::findRate(double value) {
+  auto currentTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+      std::chrono::system_clock::now().time_since_epoch());
+  auto elapsedTime = currentTime - m_lastTime;
+  m_lastTime = currentTime;
+
+  return (value - m_lastValue) / (elapsedTime.count() / 1000.0);
+}
+
+}  // namespace frc846::math
