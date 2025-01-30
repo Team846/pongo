4eaf1886b8816cffb7fa039782dc3f52e46e94cd
VyaasBaskar <vyaasbaskar@gmail.com>
2025-01-29 17:24:01 -0800

    Place drive to subtract in right place

--- src/frc846/cpp/frc846/robot/swerve/drive_to_point_command.cc
+++ src/frc846/cpp/frc846/robot/swerve/drive_to_point_command.cc
@@ -18,14 +18,10 @@ DriveToPointCommand::DriveToPointCommand(DrivetrainSubsystem* drivetrain,
 
 void DriveToPointCommand::Initialize() {
   Log("DriveToPointCommand initialized");
-  start_point_ = drivetrain_->GetReadings().estimated_pose.position;
-  is_decelerating_ = false;
+  start_point_ = drivetrain_->GetReadings().pose.position;
 }
 
 void DriveToPointCommand::Execute() {
-  const auto [target_point_overriden, is_function_overriden] = GetTargetPoint();
-  if (is_function_overriden) target_ = target_point_overriden;
-
   DrivetrainReadings dt_readings{drivetrain_->GetReadings()};
 
   DrivetrainAccelerationControlTarget dt_target{
@@ -35,35 +31,24 @@ void DriveToPointCommand::Execute() {
   };
 
   units::second_t t_decel =
-      ((dt_readings.estimated_pose.velocity.magnitude() - target_.velocity) /
+      ((dt_readings.pose.velocity.magnitude() - target_.velocity) /
           max_deceleration_);
-  units::foot_t stopping_distance = units::math::abs(
-      (dt_readings.estimated_pose.velocity.magnitude() * t_decel) -
-      ((max_deceleration_ * t_decel * t_decel) / 2.0));
-  Graph("stopping_distance", stopping_distance);
+  units::foot_t stopping_distance =
+      units::math::abs(dt_readings.pose.velocity.magnitude() * t_decel) -
+      ((max_deceleration_ * t_decel * t_decel) / 2.0);
 
-  auto dist_left =
-      (target_.point - dt_readings.estimated_pose.position).magnitude();
-
-  if (dist_left - drivetrain_->GetPreferenceValue_unit_type<units::inch_t>(
-                      "drive_to_subtract") <=
+  if ((target_.point - start_point_).magnitude() -
+          drivetrain_->GetPreferenceValue_unit_type<units::inch_t>(
+              "drive_to_subtract") <=
       stopping_distance) {
-    is_decelerating_ = true;
-
-    dt_target.accel_dir =
-        dt_readings.estimated_pose.velocity.angle(true) + 180_deg;
-
-    double decel_scale_factor = 1.0;
-    if (dist_left > 3_in) {
-      decel_scale_factor = units::math::abs(stopping_distance / dist_left);
+    if (dt_readings.pose.velocity.magnitude() < 4_fps &&
+        target_.velocity > 1_fps) {
+      dt_target.accel_dir = dt_readings.pose.velocity.angle(true) + 180_deg;
+    } else {
+      dt_target.accel_dir =
+          (dt_readings.pose.position - start_point_).angle(true);
     }
-    dt_target.linear_acceleration = max_deceleration_ * decel_scale_factor;
-    Graph("stopping", true);
-  } else if (dt_readings.estimated_pose.velocity.magnitude() < max_speed_) {
-    dt_target.linear_acceleration = max_acceleration_;
-    dt_target.accel_dir =
-        (target_.point - dt_readings.estimated_pose.position).angle(true);
-    Graph("stopping", false);
+    dt_target.linear_acceleration = max_deceleration_;
   } else {
     if (dt_readings.pose.velocity.magnitude() < max_speed_)
       dt_target.linear_acceleration = max_acceleration_;
@@ -71,8 +56,7 @@ void DriveToPointCommand::Execute() {
       dt_target.linear_acceleration = 0_fps_sq;
 
     dt_target.accel_dir =
-        (target_.point - dt_readings.estimated_pose.position).angle(true);
-    Graph("stopping", false);
+        (target_.point - dt_readings.pose.position).angle(true);
   }
 
   dt_target.angular_velocity = drivetrain_->ApplyBearingPID(target_.bearing);
@@ -86,12 +70,10 @@ void DriveToPointCommand::End(bool interrupted) {
 }
 
 bool DriveToPointCommand::IsFinished() {
-  auto current_point = drivetrain_->GetReadings().estimated_pose.position;
+  auto current_point = drivetrain_->GetReadings().pose.position;
   return (current_point - start_point_).magnitude() >=
-             (target_.point - start_point_).magnitude() ||
-         (is_decelerating_ &&
-             drivetrain_->GetReadings().estimated_pose.velocity.magnitude() <
-                 1_fps);
+         (target_.point - start_point_).magnitude();
+  // TODO: add bump sensor
 }
 
 }  // namespace frc846::robot::swerve
